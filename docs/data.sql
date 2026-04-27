0d533bd5ca0187e30838494d698470dcf198c0fb
LIAKHEVYCH LIUDMYLA <liudmyla.liakhevych@viacesi.fr>
2026-04-27 16:23:47 +0200

    added insert in database

--- docs/data.sql
+++ docs/data.sql
@@ -12,29 +12,37 @@ CREATE TABLE users (
   deleted_at DATETIME NULL
 );
 
+INSERT INTO users (name, surname, email, password)
+VALUES ('Johny', 'Jein', 'jhony@mail.com', '123456');
+
 CREATE TABLE resources (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  resource_id INT AUTO_INCREMENT PRIMARY KEY,
+  user_id INT,
+  category_id INT,
+
   popular INT AUTO_INCREMENT,
   favori INT  AUTO_INCREMENT,
   save INT  AUTO_INCREMENT,
-  user_id INT,
-  category_id INT,
 
   favorites BOOLEAN DEFAULT FALSE,
   description TEXT,
   content TEXT,
   
 
-  type ENUM('video','music','photo','paid','game'),
+  type ENUM('video','photo','game'),
   status ENUM('pending','approved','rejected') DEFAULT 'pending',
-
+  restreint BOOLEAN,
   date_creation DATETIME DEFAULT CURRENT_TIMESTAMP,
   date_publication DATETIME NULL,
   deleted_at DATETIME NULL,
 
-  FOREIGN KEY (user_id) REFERENCES users(id),
-  FOREIGN KEY (category_id) REFERENCES categories(id)
+  FOREIGN KEY (user_id) REFERENCES users(user_id),
+  FOREIGN KEY (category_id) REFERENCES categories(category_id)
 );
+
+
+INSERT INTO resources (user_id, category_id, description, content, type)
+VALUES (1, 1, 'My first video', 'video.mp4', 'video');
 
 
 CREATE TABLE comments (
@@ -46,22 +54,31 @@ CREATE TABLE comments (
   FOREIGN KEY (user_id) REFERENCES users(id)
 );
 
+INSERT INTO comments (user_id, resource_id, content)
+VALUES (1, 1, 'Nice video!');
 CREATE TABLE categories (
-  category_id INT AUTO_INCREMENT PRIMARY KEY,
+
+category_id INT AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(100),
   resource_id INT,
 
-  FOREIGN KEY (resource_id) REFERENCES recources(id)
+  FOREIGN KEY (resource_id) REFERENCES recources(resource_id)
 );
+INSERT INTO categories (name)
+VALUES ('Video'), ('Photo');
 
 CREATE TABLE statistiques (
   statistique_id INT AUTO_INCREMENT,
+  resource_id INT,
   date_creation DATETIME DEFAULT CURRENT_TIMESTAMP,
-  video_popular INT,
-  photo popular INT,
-  video_save INT,
-  photo_save INT,
-  video_favori INT, 
-  photo_favori INT
-
+  video_popular DEFAULT 0,
+  photo popular DEFAULT 0,
+  video_save DEFAULT 0,
+  photo_save DEFAULT 0,
+  video_favori DEFAULT 0, 
+  photo_favori DEFAULT 0
+  FOREIGN KEY (resource_id) REFERENCES recources(resource_id)
 );
+
+INSERT INTO statistiques (resource_id, views, favorites, saves)
+VALUES (1, 10, 2, 1);
